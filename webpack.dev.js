ad772296710a4b1d6fb6eb1139f8386f23c5427b
Philip Watson <philip.watson@adscale.co.nz>
2019-04-17 15:06:16 +1200

    Add developer workspace that works with webpack's dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,11 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    }
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: ['./dist', './dev', './test/creative'],
+    openPage: 'index.html',
+    open: true
+  }
 });
