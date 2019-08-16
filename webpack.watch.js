c13cc046d9c48c29209bcb68dce5e1a8ae18e2f9
Philip Watson <philip.watson@adscale.co.nz>
2019-08-16 14:31:23 +1200

    Add clean & watch npm scripts

---
+++ webpack.watch.js
@@ -0,0 +1,12 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = common.map(c => merge(c, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
+  watchOptions: {
+
+    ignored: /node_modules/
+  }
+}));
