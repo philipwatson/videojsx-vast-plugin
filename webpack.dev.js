b6cacbb73323d3aae39ff32e4a01082afcfd51b5
Philip Watson <philip.watson@adscale.co.nz>
2022-08-04 16:42:57 +1200

    update webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,33 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('node:path');
+
+
+const devServerOptions = {
+  devServer: {
+    open: 'index.html',
+    port: 9999,
+    // host: '127.0.0.1',
+    static: [
+      {
+        directory: path.resolve(__dirname, "dev")
+      },
+      {
+        directory: path.resolve(__dirname, "test/creative")
+      }
+    ],
+    devMiddleware: {
+      publicPath: "/bundle/",
+    }
+  }
+};
+
+// From https://webpack.js.org/configuration/dev-server/
+// "Be aware that when exporting multiple configurations only the devServer options for the first configuration will be
+// taken into account and used for all the configurations in the array."
+common[0] = merge(common[0], devServerOptions);
 
 module.exports = common.map(c => merge(c, {
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    publicPath: "/bundle/",
-    contentBase: ['./dev', './test/creative'],
-    openPage: 'index.html',
-    open: true,
-    port: 9999
-  }
 }));
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,32 @@
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CompressionPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = common.map(c => merge(c, {
     mode: 'production',
     plugins: [
         new CompressionPlugin({
-            test: /\.js/,
+            test: /\.js/
         }),
     ],
 
     performance: {
         maxEntrypointSize: 850000,
         maxAssetSize: 850000
-    }
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
 }));
